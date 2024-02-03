24c6e2e180e5f92f9725396400adbc9a507a9166
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2024-02-03 16:33:06 +0000

    changes

---
+++ scripts/dark-vibrant-desktop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the content of the .desktop file
+desktop_file_content="[Desktop Entry]
+Name=Dark-Vibrant-Desktop
+Comment=Dark-Vibrant-Desktop of DWM
+Exec=$HOME/.config/dark-vibrant-desktop/scripts/./autostart
+Type=Application"
+
+# Path to the .desktop file
+desktop_file_path="./"
+
+# Write the content to the .desktop file
+echo "$desktop_file_content" > "$desktop_file_path"
+
+# Make the .desktop file executable
+chmod +x "$desktop_file_path"
