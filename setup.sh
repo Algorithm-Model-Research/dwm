2b5a8c9ccaf19b1c0bb6c475cced26315bfa053c
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2024-02-03 16:57:08 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ sudo apt-get install libimlib2-dev &&
 rm -rf ~/.config/dark-vibrant-desktop/ &&
 cp -r ../dark-vibrant-desktop ~/.config/ &&
 rm -rf ~/.config/dark-vibrant-desktop/.git/ &&
-sh ~/.config/dark-vibrant-desktop/scripts/dark-vibrant-desktop.sh &&
+cd ~/.config/dark-vibrant-desktop/scripts/ &&
+./dark-vibrant-desktop.sh &&
 sudo cp ~/.config/dark-vibrant-desktop/scripts/dark-vibrant-desktop.desktop /usr/share/xsessions/ &&
 cp -r ~/.config/dark-vibrant-desktop/fonts/ ~/.local/share/fonts &&
 cd ~/.config/dark-vibrant-desktop/dwm &&
