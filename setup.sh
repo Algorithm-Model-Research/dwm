d1a259cd3a631f5bfed09769468989ce5794a2ef
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2024-02-03 16:06:24 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y xbacklight &&
+sudo apt-get install -y xbacklight feh &&
 sudo apt-get install x11-xserver-utils &&
 sudo apt install build-essential libx11-dev libxinerama-dev sharutils libxft-dev &&
 sudo apt-get install libimlib2-dev &&
