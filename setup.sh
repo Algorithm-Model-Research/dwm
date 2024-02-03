033a11d48f88092bba9a66e81656f0f46b4b5cef
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2024-02-03 14:22:22 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ sudo apt-get install -y xbacklight &&
 sudo apt-get install x11-xserver-utils &&
 sudo apt install build-essential libx11-dev libxinerama-dev sharutils libxft-dev &&
 
-cp ../dark-vibrant-desktop ~/.config/ &&
+cp -r ../dark-vibrant-desktop ~/.config/ &&
 cp ~/.config/dark-vibrant-desktop/scripts/dark-vibrant-desktop.desktop /usr/share/xsessions/ &&
-cp ~/.config/dark-vibrant-desktop/fronts/ ~/.local/share/fonts && 
+cp -r ~/.config/dark-vibrant-desktop/fronts/ ~/.local/share/fonts && 
 cd ~/.config/dark-vibrant-desktop/dwm &&
 sudo make install
