75c3f306be22dff10cf9e084537dce0fe770130b
Ronaldson Bellande <47253433+RonaldsonBellande@users.noreply.github.com>
2024-02-03 15:18:21 +0000

    changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y xbacklight && 
+sudo apt-get install -y xbacklight &&
 sudo apt-get install x11-xserver-utils &&
 sudo apt install build-essential libx11-dev libxinerama-dev sharutils libxft-dev &&
 sudo apt-get install libimlib2-dev &&
@@ -7,6 +7,6 @@ rm -rf ~/.config/dark-vibrant-desktop/ &&
 cp -r ../dark-vibrant-desktop ~/.config/ &&
 rm -rf ~/.config/dark-vibrant-desktop/.git/ &&
 sudo cp ~/.config/dark-vibrant-desktop/scripts/dark-vibrant-desktop.desktop /usr/share/xsessions/ &&
-cp -r ~/.config/dark-vibrant-desktop/fonts/ ~/.local/share/fonts && 
+cp -r ~/.config/dark-vibrant-desktop/fonts/ ~/.local/share/fonts &&
 cd ~/.config/dark-vibrant-desktop/dwm &&
 sudo make install
